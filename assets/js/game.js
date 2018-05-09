8c88db65fe176f7caac0aa27ea0d26e80a2476e9
Nick Tan <nctan1002@gmail.com>
2018-05-09 05:56:48 -0400

    Added sounds, ability to replace pipes, rough game flow

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,8 +1,8 @@
-// To do: water flow set new direction
-// To do: end sections
 // To do: countdown - setinterval
 // To do: levels, score, difficulty ramp
 // To do: fix cross img
+// To do: prevent all events till start clicked
+// To do: display board function for all html changes (fix score)
 
 $(document).ready(function () {
 
@@ -13,39 +13,53 @@ $(document).ready(function () {
     const TIME_TO_START = 8000;
     const WATER_SPEED = 3000;
     const WON = false;
+    let score = 0;
+
+    // sounds
+    const placed = new Audio("assets/sounds/yes.mp3");
+    const notAllowed = new Audio("assets/sounds/no.mp3");
+    const lose = new Audio("assets/sounds/lose.mp3");
+    const start = new Audio("assets/sounds/start.mp3");
 
     // pieces, properties are directions for incoming flow and values are outgoing flow
     const pieceTypes = {
         // curved pieces
         topRight: {
             south: "west",
-            west: "south"
+            west: "south",
+            passed: false
         },
         bottomRight: {
             north: "west",
-            west: "north"
+            west: "north",
+            passed: false
         },
         topLeft: {
             south: "east",
-            east: "south"
+            east: "south",
+            passed: false
         },
         bottomLeft: {
             north: "east",
-            east: "north"
+            east: "north",
+            passed: false
         },
         vertical: {
             south: "north",
-            north: "south"
+            north: "south",
+            passed: false
         },
         horizontal: {
             east: "west",
-            west: "east"
+            west: "east",
+            passed: false
         },
-        cross: { 
+        cross: {
             north: "south",
-            south: "north", 
+            south: "north",
             east: "west",
-            west: "east"
+            west: "east",
+            passed: false
         },
         // topRight: { north: false, east: false, south: true, west: true },
         // bottomRight: { north: true, east: false, south: false, west: true },
@@ -63,6 +77,7 @@ $(document).ready(function () {
     function Pipe(name, type) {
         this.name = name;
         this.flow = type;
+        this.passed = type.passed;
         // this.north = type.north;
         // this.east = type.east;
         // this.south = type.south;
@@ -81,7 +96,7 @@ $(document).ready(function () {
             this.img = "<img src='assets/images/curve.png' class='bottom-left'>";
         } else if (name == "cross") {
             // this.img = "<img src='assets/images/straight.png' class='straight'><img src='assets/images/straight.png' class='vertical-straight'>";
-            this.img = "<img src='assets/images/straight.gif' class='vertical-straight'>";
+            this.img = "<img src='assets/images/cross.png' class='vertical-straight'>";
         }
     }
     // test = new Pipe(pieceTypes.topRight);
@@ -140,8 +155,6 @@ $(document).ready(function () {
             let y = Math.floor(Math.random() * ROWS);
             if (text === "start") {
                 x = 0;
-                this.x = x;
-                this.y = y;
             } else if (text === "end") {
                 x = COLS - 1;
             } else {
@@ -152,6 +165,7 @@ $(document).ready(function () {
             // this.path[x] = [];
             // this.path[x][y] = new Pipe("horizontal", pieceTypes.horizontal);
             this.addToPath(x, y, new Pipe("horizontal", pieceTypes.horizontal));
+            this.path[x][y].passed = true;
             console.log(text + " x " + x);
             console.log(text + " y " + y);
             let row = $("#board").find(".row[data-index='" + y + "']");
@@ -186,6 +200,7 @@ $(document).ready(function () {
                 temp_data[y] = pipe;
             }
             this.path[x] = temp_data;
+            console.log(this.path);
             // this.path[x] = { [y]: pipe };
         }
         this.checkConnected = function () {
@@ -198,6 +213,7 @@ $(document).ready(function () {
                 return true;
             } else if (this.direction == "east" && this.x + 1 < COLS) {
                 if (pipe[this.x + 1][this.y].flow.hasOwnProperty("west")) {
+                    this.path[this.x + 1][this.y].passed = true;
                     this.setDirection(pipe[this.x + 1][this.y].flow.west);
                     // update position of water to next connected pipe
                     this.x += 1;
@@ -206,27 +222,33 @@ $(document).ready(function () {
                 }
             } else if (this.direction == "west" && this.x - 1 >= 0) {
                 if (pipe[this.x - 1][this.y].flow.hasOwnProperty("east")) {
+                    this.path[this.x - 1][this.y].passed = true;
                     this.setDirection(pipe[this.x - 1][this.y].flow.east);
                     this.x -= 1;
-                    console.log("x: " + this.x + "y: " + this.y);
+                    console.log("x: " + this.x + " | y: " + this.y);
                     return true;
                 }
             } else if (this.direction == "north" && this.y - 1 >= 0) {
                 if (pipe[this.x][this.y - 1].flow.hasOwnProperty("south")) {
+                    this.path[this.x][this.y - 1].passed = true;
                     this.setDirection(pipe[this.x][this.y - 1].flow.south);
                     this.y -= 1;
-                    console.log("x: " + this.x + "y: " + this.y);
+                    console.log("x: " + this.x + " | y: " + this.y);
                     return true;
                 }
             } else if (this.direction == "south" && this.y + 1 < ROWS) {
                 if (pipe[this.x][this.y + 1].flow.hasOwnProperty("north")) {
+                    this.path[this.x][this.y + 1].passed = true;
                     this.setDirection(pipe[this.x][this.y + 1].flow.north);
                     this.y += 1;
-                    console.log("x: " + this.x + "y: " + this.y);
+                    console.log("x: " + this.x + " | y: " + this.y);
                     return true;
                 }
             }
+            // lost
             console.log("You lose");
+            $("#bg-music").get(0).pause();
+            lose.play();
             return false;
         }
         let _this = this;
@@ -235,37 +257,50 @@ $(document).ready(function () {
                 // if (_this.checkConnected(_this.x+1, _this.y)) {
                 if (_this.checkConnected()) {
                     console.log("connected");
+                    score += 100;
+                    $("#score").text(score);
                 } else {
                     clearInterval(flow);
                 }
             }, WATER_SPEED);
         }
         this.setDirection("east");
-        this.end = this.generatePoint("end");
+        this.resetPath();
         this.start = this.generatePoint("start");
+        this.end = this.generatePoint("end");
+        this.x = this.start.x;
+        this.y = this.start.y;
     }
 
     const water = new Water();
-    water.resetPath();
     console.log("start direction: " + water.direction);
 
     $("#board").on("click", ".block", function () {
-        if ($(this).find("img").length == 0) {
-            $(this).html($(".current").find("img").clone());
-            let rowIndex = $(this).data("index");
-            let colIndex = $(this).parent().data("index");
-            let type = pile.pile[VISIBLE_PIECES - 1].name;
+        let rowIndex = $(this).data("index");
+        let colIndex = $(this).parent().data("index");
+        if (!water.path[rowIndex][colIndex].passed) {
+            if ($(this).find("img").length > 0) {
+                score -= 50;
+                $("#score").text(score);
+            }
+            // let type = pile.pile[VISIBLE_PIECES - 1].name;
             // console.log(pile.pile[VISIBLE_PIECES - 1]);
             // if (water.checkConnected(rowIndex, colIndex, pile.pile[VISIBLE_PIECES - 1])) {
+            $(this).html($(".current").find("img").clone());
             water.addToPath(rowIndex, colIndex, pile.pile[VISIBLE_PIECES - 1]);
             // }
             pile.removePiece();
             pile.generatePiece();
+            placed.play();
+            placed.currentTime = 0;
             // console.log($(this).parent().data("index"));
             // console.log($(this).data("index"));
             updateBoardPile();
             // let img = $(this).find("img").attr("src");
             // $(this).find("img").attr({src: img.replace("png", "gif")});
+        } else {
+            notAllowed.play();
+            notAllowed.currentTime = 0;
         }
     });
 
@@ -303,13 +338,21 @@ $(document).ready(function () {
                     class: "block",
                     "data-index": z
                 });
-                $(block).text("P");
+                // $(block).text("P");
                 board.find("#row" + i).append(block);
             }
         }
     }
 
-    let flow = setTimeout(function () {
-        water.startFlow();
-    }, TIME_TO_START);
+    $("#start").on("click", function () {
+        start.play();
+        $(this).hide();
+        setTimeout(function () {
+            let flow = setTimeout(function () {
+                water.startFlow();
+            }, TIME_TO_START);
+            $("#bg-music").get(0).play(); // get(0) audio object from element
+        }, start.duration * 1000);
+    })
+
 });
